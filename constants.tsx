bfb7ca8e13d1763a3a96c47b75db84ed9e78302a
Nathaniel Mina <nate@dom-i-nate.pro>
2026-05-15 05:00:09 -0400

    Update Gemini model to gemini-2.0-flash for API compatibility

--- constants.tsx
+++ constants.tsx
@@ -5,7 +5,7 @@ export const VisionWeaverIcon = (
     <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
     <path strokeLinecap="round" strokeLinejoin="round" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
     <path strokeLinecap="round" strokeLinejoin="round" d="M19.463 7.564C18.49 5.897 17.064 4.543 15.364 3.5M4.537 7.564C5.51 5.897 6.936 4.543 8.636 3.5M12 21v-1.5M12 3V1.5M4.5 12H3m18 0h-1.5" />
- </svg>
+  </svg>
 );
 
 
@@ -18,7 +18,7 @@ export const CameraIcon = (
 
 export const NoCameraIcon = (
   <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12 text-slate-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M13.52 3.085A2 2 0 0012.07 2H7.93a2 2 0 00-1.45.585L4.5 4.585M4.5 4.585a2 2 0 00-1.45 1.45L2 7.93v8.14a2 2 0 00.585 1.45l1.915 1.915a2 2 0 001.45.585h8.14a2 2 0 001.45-.585l1.915-1.915a2 2 0 00.585-1.45V7.93a2 2 0 00-.585-1.45L16.07 4.5M4.5 4.585L16.07 4.5m-11.57 0L2 6.07M19 12a4 4 0 11-8 0 4 4 0 018 0zm-4-7h.01M22 22L2 2" />
+    <path strokeLinecap="round" strokeLinejoin="round" d="M13.52 3.085A2 2 0 0012.07 2H7.93a2 2 0 00-1.45.585L4.5 4.585M4.5 4.585a2 2 0 00-1.45 1.45L2 7.93v8.14a2 2 0 00.585 1.45l1.915 1.915a2 2 0 001.45.585h8.14a2 2 0 001.45-.585l1.915-1.915a2 2 0 00.585-1.45V7.93a2 2 0 00-.585-1.45L13.52 3.085M22 22L2 2m5 5h8m4 4v4m-4-4h4" />
   </svg>
 );
 
@@ -31,7 +31,7 @@ export const MagnifyingGlassIcon = (
 
 export const StoryIcon = (
   <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 sm:h-6 sm:w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-     <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.25278C12 6.25278 15.5254 2.72742 19.0508 6.25278C22.5761 9.77813 19.0508 17.0003 12 17.0003C4.94921 17.0003 1.42385 9.77813 4.94921 6.25278C8.47456 2.72742 12 6.25278 12 6.25278Z" />
+     <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.25278C12 6.25278 15.5254 2.72742 19.0508 6.25278C22.5761 9.77813 19.0508 17.0003 12 17.0003C4.94921 17.0003 1.42385 9.77813 4.94921 6.25278C8.47457 2.72742 12 6.25278 12 6.25278Z" />
     <path strokeLinecap="round" strokeLinejoin="round" d="M12 17V21M7 21H17M12 4V2M5 5L3.5 3.5M19 5L20.5 3.5" />
   </svg>
 );
@@ -44,7 +44,7 @@ export const ImageIcon = (
 
 export const SpeakerPlayIcon = (
   <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-    <path d="M6.293 4.293a1 1 0 011.414 0L12 8.586l4.293-4.293a1 1 0 111.414 1.414L13.414 10l4.293 4.293a1 1 0 01-1.414 1.414L12 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L10.586 10 6.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
+    <path d="M6.293 4.293a1 1 0 011.414 0L12 8.586l4.293-4.293a1 1 0 111.414 1.414L13.414 10l4.293 4.293a1 1 0 01-1.414 1.414L12 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L10.586 10 6.293 5.707a1 1 0 010-1.414z" />
     <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM4 10a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1z"  />
      <path d="M9.383 5.077A1 1 0 008 6v8a1 1 0 001.383.923l6-4a1 1 0 000-1.846l-6-4z" />
   </svg>
@@ -52,7 +52,7 @@ export const SpeakerPlayIcon = (
 
 export const SpeakerWaveIcon = (
   <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-    <path strokeLinecap="round" strokeLinejoin="round" d="M15.536 8.464a5 5 0 010 7.072M18.364 5.636a9 9 0 010 12.728M12 9v6m-4-3h2m4 0h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V9a2 2 0 012-2h2a2 2 0 012 2zm10-5a9 9 0 00-9-9 9 9 0 00-9 9v6a9 9 0 009 9 9 9 0 009-9V4z" />
+    <path strokeLinecap="round" strokeLinejoin="round" d="M15.536 8.464a5 5 0 010 7.072M18.364 5.636a9 9 0 010 12.728M12 9v6m-4-3h2m4 0h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V9a2 2 0 012-2h2a2 2 0 012 2v2m0 0V9a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H8a2 2 0 01-2-2V9" />
      <path strokeLinecap="round" strokeLinejoin="round" d="M11.9999 12.0001L12 6.00008M12 6.00008C10.7766 6.00008 9.60199 5.30932 8.82833 4.17171M12 6.00008C13.2232 6.00008 14.398 5.30932 15.1717 4.17171M15.9999 14.0001C15.4999 15.5001 14.5 16.0001 12.5 16.0001C10.5 16.0001 9.5 15.5001 8.99995 14.0001" />
   </svg>
 );
@@ -67,5 +67,5 @@ export const SpeakerXMarkIcon = (
 
 
 // Model names
-export const GEMINI_TEXT_MODEL = 'gemini-2.5-flash-preview-04-17';
+export const GEMINI_TEXT_MODEL = 'gemini-2.0-flash';
 // export const GEMINI_IMAGE_MODEL = 'imagen-3.0-generate-002'; // If image generation was needed
